1ac00791e0b76f3e22a3a7651cf6b0958ea311b8
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2023-12-04 10:35:10 -0500

    actual content

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function handleModal(id) {
     let modal = document.querySelector(`#${String(id)}`);
     modal.classList.toggle("is-active")
 }
+
+function submitLogin(e) {
+    let email = e.toLowerCase() ? e.toLowerCase() : "lol@empty.com"
+    console.log(email)
+    var params = new URLSearchParams(location.search);
+    params.set('email', email);
+    window.location.search = params.toString();
+}
+
+const app = Vue.createApp({
+    data() {
+        let urlp = new URLSearchParams(window.location.search).get('email') ? new URLSearchParams(window.location.search).get('email') : "lol@empty.com"
+        return {
+            valid: urlp === "death"
+        }
+    }
+})
